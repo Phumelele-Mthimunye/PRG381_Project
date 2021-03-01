ace80fa79ee69dc94dca5abc91a70e3d2e8945c9
Mar-debug <72023670+Mar-debug@users.noreply.github.com>
2021-03-01 11:26:40 +0200

    added GetAddress method

--- BusinessLogicLayer/DataHandler.java
+++ BusinessLogicLayer/DataHandler.java
@@ -1,8 +1,25 @@
 package BusinessLogicLayer;
+import DataAccessLayer.*;
+import java.io.*;
+import java.util.*;
+
 
 public class DataHandler {
-    public DataHandler(){}
+    String filepath = "C:\\Users\\Aura\\Desktop\\PRG381 Project\\Address.txt";
+    public List<Address> GetAddress() throws FileNotFoundException{
+        List<Address> aList = new ArrayList<Address>();
+        
+        File file = new File(filepath);
+        Scanner sc = new Scanner(file);
+        while(sc.hasNextLine()){
+            String[] line = sc.nextLine().split(",");
+            aList.add(new Address(AddressID, UnitNo, Street, Suburb, City, PhoneNo));
 
-    String connect = "Data Source =.; Initial Catalog= DeliciousCateringDB; Integrated Security = SSPI";
+
+        }
+        sc.close();
+        return aList;
+
+
     
 }
