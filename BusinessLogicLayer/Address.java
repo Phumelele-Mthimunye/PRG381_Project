e072b692658cff79b079aae76ee7c1716602c696
Mar-debug <72023670+Mar-debug@users.noreply.github.com>
2021-03-06 15:08:18 +0200

    Changed some errors

--- BusinessLogicLayer/Address.java
+++ BusinessLogicLayer/Address.java
@@ -75,10 +75,6 @@ public class Address {
         this.City = City;
         this.PhoneNo = PhoneNo;
     }
-<<<<<<< HEAD:BusinessLogicLayer/Address.java
-=======
-
->>>>>>> 650643f524757923b4beb809fd2dd7ec02511700:DataAccessLayer/Address.java
     public List<Address> addresses( ) throws FileNotFoundException{
         DataHandler dh = new DataHandler();
         List<Address> aList = dh.GetAddress();
